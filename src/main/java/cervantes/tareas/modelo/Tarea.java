d4e07dfe54607c0994480fb173ca912df786a06e
Oscar <oscarcr189@outlook.com>
2023-10-25 21:03:57 -0600

    Proyecto tareas2

---
+++ src/main/java/cervantes/tareas/modelo/Tarea.java
@@ -0,0 +1,15 @@
+package cervantes.tareas.modelo;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Tarea {
+}
