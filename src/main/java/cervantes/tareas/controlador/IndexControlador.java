6b49636b331f069b84149d9ade9b2e7b545e161a
Oscar <oscarcr189@outlook.com>
2023-10-27 20:42:52 -0600

    Proyecto tareas5 pom.xml

---
+++ src/main/java/cervantes/tareas/controlador/IndexControlador.java
@@ -0,0 +1,14 @@
+package cervantes.tareas.controlador;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class IndexControlador implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
