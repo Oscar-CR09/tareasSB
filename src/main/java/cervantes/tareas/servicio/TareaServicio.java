040efefe1fdcfc6b04542f2fd3fc91ade15575c8
Oscar <oscarcr189@outlook.com>
2023-10-27 19:29:00 -0600

    Proyecto tareas4 servicio

--- src/main/java/cervantes/tareas/servicio/TareaServicio.java
+++ src/main/java/cervantes/tareas/servicio/TareaServicio.java
@@ -1,29 +1,42 @@
 package cervantes.tareas.servicio;
 
 import cervantes.tareas.modelo.Tarea;
+import cervantes.tareas.repositorio.TareaRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class TareaServicio implements ITareaServicio{
 
+    @Autowired
+    private TareaRepositorio tareaRepositorio;
 
     @Override
     public List<Tarea> listarTareas() {
-        return null;
+
+        return tareaRepositorio.findAll();
     }
 
     @Override
     public Tarea buscarTareaPorId(Integer idTarea) {
-        return null;
+        Tarea tarea =tareaRepositorio.findById(idTarea).orElse(null);
+        return tarea;
+
+
     }
 
     @Override
     public void guardarTarea(Tarea tarea) {
+
+        tareaRepositorio.save(tarea);
 
     }
 
     @Override
     public void elimniarTarea(Tarea tarea) {
 
+        tareaRepositorio.delete(tarea);
     }
 }
