2255a4095d71174ca8db64ea90346c6242a1afeb
Oscar <oscarcr189@outlook.com>
2023-10-29 21:02:49 -0600

    Proyecto tareas7 configuracion1

--- src/main/java/cervantes/tareas/controlador/IndexControlador.java
+++ src/main/java/cervantes/tareas/controlador/IndexControlador.java
@@ -1,16 +1,60 @@
 package cervantes.tareas.controlador;
 
+import cervantes.tareas.modelo.Tarea;
+import cervantes.tareas.servicio.TareaServicio;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.cell.PropertyValueFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.scene.control.*;
+
 
 @Component
 public class IndexControlador implements Initializable {
 
+    private static final Logger logger = LoggerFactory.getLogger(IndexControlador.class);
+
+    @Autowired
+    private TareaServicio tareaServicio;
+
+    @FXML
+    private TableView<Tarea> tareaTabla;
+
+    @FXML
+    private TableColumn<Tarea,Integer> idTareaColumna;
+
+    @FXML
+    private TableColumn<Tarea,String> nombreTareaColumna;
+
+    @FXML
+    private TableColumn<Tarea,String> responsableColumna;
+
+    @FXML
+    private TableColumn<Tarea,String> estatusColumna;
+
+    private final ObservableList<Tarea> tareaList = FXCollections.observableArrayList();
+
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        tareaTabla.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        configurarColumnas();
+    }
+
+
+    private void configurarColumnas(){
+        idTareaColumna.setCellValueFactory(new PropertyValueFactory<>("idTarea"));
+        nombreTareaColumna.setCellValueFactory(new PropertyValueFactory<>("nombreTarea"));
+        responsableColumna.setCellValueFactory(new PropertyValueFactory<>("responsable"));
+        estatusColumna.setCellValueFactory(new PropertyValueFactory<>("estatus"));
 
     }
 }
