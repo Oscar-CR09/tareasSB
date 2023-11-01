539eb273757362f6e0f97665159b156ef0a6bea7
Oscar <oscarcr189@outlook.com>
2023-10-31 19:51:23 -0600

    Proyecto tareas10 boton agregartarea

--- src/main/java/cervantes/tareas/controlador/IndexControlador.java
+++ src/main/java/cervantes/tareas/controlador/IndexControlador.java
@@ -41,6 +41,15 @@ public class IndexControlador implements Initializable {
 
     private final ObservableList<Tarea> tareaList = FXCollections.observableArrayList();
 
+    @FXML
+    private TextField nombreTareaTexto;
+
+    @FXML
+    private TextField responsableTexto;
+
+    @FXML
+    private TextField estatusTexto;
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -68,5 +77,43 @@ public class IndexControlador implements Initializable {
 
     }
 
-    
+    public void agregarTarea(){
+
+        if (nombreTareaTexto.getText().isEmpty()){
+            mostrarMensaje("Error Validación", "Deben proporcionar una tarea ");
+            nombreTareaTexto.requestFocus();
+            return;
+        }else {
+            var tarea = new Tarea();
+            recolectarDatosFormulario(tarea);
+            tareaServicio.guardarTarea(tarea);
+            mostrarMensaje("Informacion", "Tarea agregada");
+            limpiarFormulario();
+            listarTareas();
+
+        }
+    }
+
+    private void recolectarDatosFormulario(Tarea tarea){
+        tarea.setNombreTarea(nombreTareaTexto.getText());
+        tarea.setResponsable(responsableTexto.getText());
+        tarea.setEstatus(estatusTexto.getText());
+
+    }
+
+    private void limpiarFormulario(){
+        nombreTareaTexto.clear();
+        responsableTexto.clear();
+        estatusTexto.clear();
+    }
+
+    private void mostrarMensaje(String titulo, String mensaje){
+        Alert alerta = new Alert(Alert.AlertType.INFORMATION);
+        alerta.setTitle(titulo);
+        alerta.setHeaderText(null);
+        alerta.setContentText(mensaje);
+        alerta.showAndWait();
+
+    }
+
 }
